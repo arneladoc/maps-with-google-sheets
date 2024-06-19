8817b04e3eadd8c4b0597ad5d7633567268e40a3
Arnela <108413190+arneladoc@users.noreply.github.com>
2024-06-19 14:50:23 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,6 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = null ;
+// data stored in csv folder on GitHub, not Google Sheets
+var googleDocURL = null;
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
